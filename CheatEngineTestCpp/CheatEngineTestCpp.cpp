69445c9b6da0f5bf9a15f6104d4f61e60581de61
Chris <cdesch@gmail.com>
2017-08-20 13:18:03 -0400

    Initial POC with simple Loop

--- CheatEngineTestCpp/CheatEngineTestCpp.cpp
+++ CheatEngineTestCpp/CheatEngineTestCpp.cpp
@@ -2,10 +2,29 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main()
 {
+	printf("Hello World!\n");
+	system("PAUSE");
+	string mystr;
+	int counter = 0;
+	int countByFives = 0; 
+	mystr = "";
+	while (mystr != "e") {
+		cout << "Counter: " << counter << endl;
+		cout << "Count by Fives: " << countByFives << endl;
+		cout << "What's next? ";
+		getline(cin, mystr);
+		counter++;
+		countByFives += 5;
+	}
     return 0;
 }
 
